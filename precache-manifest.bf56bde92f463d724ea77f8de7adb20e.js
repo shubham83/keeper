8e0527723eedf2fd540db86b72e5685c53896a06
Shubham <shubhampoddar83@gmail.com>
2023-07-27 16:17:38 +0530

    Updates

---
+++ precache-manifest.bf56bde92f463d724ea77f8de7adb20e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc846b6becee746500d7fbce7dfbf6f4",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "140303718ebb369fd3fc",
+    "url": "/keeper/static/js/2.e928da4b.chunk.js"
+  },
+  {
+    "revision": "db26a171dd8e693346da",
+    "url": "/keeper/static/js/main.3c88ad4b.chunk.js"
+  },
+  {
+    "revision": "ee38aac5cc265be45431",
+    "url": "/keeper/static/js/runtime-main.d35bd170.js"
+  }
+]);
